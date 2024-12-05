79425fbe25bc3ccbbe1db4a054ee686a4a0a19c4
GabContreras <contrerasgab01@gmail.com>
2024-12-05 11:13:05 -0600

    Otros cambios en conexion asljkadsbasio

--- src/modelo/ClaseConexion.java
+++ src/modelo/ClaseConexion.java
@@ -1,15 +1,20 @@
 package modelo;
 
 import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ClaseConexion {
 
     // Variables para la cadena de conexión
     private static final String URL = "jdbc:oracle:thin:@localhost:1521:xe";
-    private static final String USUARIO = "agalo"; // Agregar usuario local antes de iniciar sesión
-    private static final String CONT = "agalo"; // Agregar contraseña local antes de iniciar sesión
+    private static final String USUARIO = ""; // Agregar usuario local antes de iniciar sesión
+    private static final String CONT = ""; // Agregar contraseña local antes de iniciar sesión
 
+    //Instancia de logger para sustituir los system out 
+    private static final Logger logger = Logger.getLogger(ClaseConexion.class.getName());
 // Constructor privado para evitar instanciación
+
     private ClaseConexion() {
         // No se permite la instanciación
     }
@@ -18,18 +23,14 @@ public class ClaseConexion {
     public static Connection getConexion() {
         try {
             // Cargar el driver JDBC (comentariado porque hoy en día no es necesario)
-//            Class.forName("oracle.jdbc.driver.OracleDriver");
 
             // Obtener la conexión en una variable
             //NOTA: sonar indica que es mejor retornar directamente la conexion
-           return DriverManager.getConnection(URL, USUARIO, CONT);
+            return DriverManager.getConnection(URL, USUARIO, CONT);
             // Retornamos la variable que tiene la conexión
         } catch (SQLException e) {
-            System.out.println("Este es el error: " + e);
+            logger.log(Level.SEVERE, "Error de SQL: ", e);
             return null;
-//        } catch (ClassNotFoundException ex) {
-//            System.out.println("Este es el error de la clase: " + ex);
-//            return null;
         }
     }
 }
